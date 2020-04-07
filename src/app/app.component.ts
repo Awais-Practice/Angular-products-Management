4bcc2758f9fbf3aa842ad18a94afef67ba8564d7
Ayyaz <you@example.com>
2020-04-07 15:49:13 +0500

    Angular Product Manegement Practice Pluralsight

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template:`<div><h1>Product Manegment</h1><div><pm-product></pm-product></div></div>`
+
+
+
+})
+export class AppComponent {
+  pageTitle = 'Angular: Getting Started';
+}
+
+
+
