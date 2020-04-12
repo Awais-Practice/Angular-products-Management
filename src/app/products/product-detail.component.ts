6911337db227c716e0b7eb07f6833986bcb95146
Ayyaz <you@example.com>
2020-04-12 23:48:56 +0500

    In this section of code router navigation back function created

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from './product';
 import { from } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,12 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailComponent implements OnInit {
 pageTitle:string ="Product Detail";
 product: IProduct;
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+      private router: Router) {
    }
 
   ngOnInit() {
     let id =this.route.snapshot.paramMap.get("id");
     console.log(id);
   }
+
+onBack(): void{
+  this.router.navigate(["/products"])
+}
 
 }
